90186a8937d8bd2f19c9e433fc7a65d5a9ad079e
irenemanas <irenemanas@gmail.com>
2021-06-13 09:41:55 +0200

    Add appendTo, remove and parent

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ $(document).ready(function () {
   $("#target1").addClass("btn-primary");
   $("#target2").css("color", "red");
   $("#target3").prop("disabled", true);
+  $("#target4").html("<em>#target4</em>");
+  $("#target5").remove();
+  $("#target2").appendTo("#right-well");
+  $("#target6").clone().appendTo("#left-well");
+  $("#target1").parent().css("background-color", "blue");
   $("#click-btn").click(function () {
     $("p").hide();
   });
